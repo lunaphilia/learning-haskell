d3f2fdf4e35ce1ed04633dcf867068d0fbae6eb7
lunaphilia <lunaphilia@gmail.com>
2014-04-20 13:38:44 +0900

    No.570まで

--- baby.hs
+++ baby.hs
@@ -1,1 +1,7 @@
 doubleMe x = x + x
+doubleUs x y = doubleMe x + doubleMe y
+doubleSmallNumber x = if x > 100 then x else x * 2
+ubleSmallNumber' x = (if x > 100 then x else x * 2) + 1
+coananO'Brien = "It's a-me, Conan O'Brien!"
+boomBangs xs = [ if x < 10 then "Boom!" else "Bang!" | x <- xs, odd x]
+
